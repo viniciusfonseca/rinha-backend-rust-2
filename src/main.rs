b0a47bf500294d19b1ed750b8fada4fe0513c57e
viniciusfonseca <vfonseca1618@gmail.com>
2024-02-07 17:00:11 -0300

    minor refactors

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,30 @@ use tokio_postgres::NoTls;
 
 mod handlers;
 
-type AppResult<T> = Result<T, Box<(dyn std::error::Error + 'static)>>;
-
 #[tokio::main]
-async fn main() -> AppResult<()> {
+async fn main() {
 
     let mut cfg = deadpool_postgres::Config::new();
-    cfg.host = Some(
-        std::env::var("DB_HOST")
-            .unwrap_or("localhost".into())
-            .to_string(),
-    );
+    cfg.host = Some("db".to_string());
     cfg.port = Some(5432);
     cfg.dbname = Some("rinhadb".to_string());
     cfg.user = Some("root".to_string());
     cfg.password = Some("1234".to_string());
-    let pool_size = std::env::var("POOL_SIZE")
-        .unwrap_or("125".to_string())
-        .parse::<usize>()
-        .unwrap();
+    let pool_size = 125;
 
     cfg.pool = deadpool_postgres::PoolConfig::new(pool_size).into();
-    println!("creating postgres pool...");
-    let pg_pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls)?;
-    println!("postgres pool succesfully created");
+    let pg_pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls)
+        .expect("error creating pg pool");
 
     let app = Router::new()
         .route("/clientes/:id/transacoes", post(handlers::inserir_transacao::handler))
         .route("/clientes/:id/extrato", get(handlers::extrato::handler))
         .with_state(pg_pool);
 
-    let http_port = std::env::var("HTTP_PORT").unwrap_or("80".into());
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:80")).await
+        .expect("error while listening to port 80");
+    
+    axum::serve(listener, app).await
+        .expect("error while serving app");
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{http_port}")).await?;
-    axum::serve(listener, app).await?;
-
-    Ok(())
 }
